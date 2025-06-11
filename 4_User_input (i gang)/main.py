95f94e2183044b7eb490ee6e9ef79298d2ed2302
Tobias Bøge Nielsen <tbn@datacompagniet.dk>
2025-06-11 12:13:48 +0200

    started user input

---
+++ 4_User_input (i gang)/main.py
@@ -0,0 +1,10 @@
+# The Input function can be used to accept input from a user
+# The string inside the function displays a prompt in the terminal 
+
+# input("Enter your name: ")
+
+# Save the input to a variable, that can be used later in the script
+# fx. in a print statement
+
+name = input("Enter your name: ")
+print(f"Your name is {name}") 
