8a77204d6a1b008a221b001bc9c92ba87e68eacf
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2025-06-13 16:57:19 +0200

    laves madlibs spil

---
+++ 4_User_input (i gang)/mad lips project game.py
@@ -0,0 +1,10 @@
+adjektive1 = input("Indsæt et adjektiv: ")
+noun = input("Indsæt et navneord: ")
+adjektive2 = input("Indsæt et adjektiv: ")
+verb = input("Indsæt et udsagsord: ")
+adjektive3 = input("Indsæt et adjektiv: ")
+
+print(f"I dag gik jeg i en {adjektive1} zoologisk have.")
+print(f"I en indhegning, så jeg {noun}")
+print(f"{noun} var {adjektive2} og {verb}")
+print(f"Jeg var {adjektive3}")
