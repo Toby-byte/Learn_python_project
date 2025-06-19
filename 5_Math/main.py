4a023e9fefa137348599120b4cc2135600a57889
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2025-06-19 22:00:23 +0200

    began python math

---
+++ 5_Math/main.py
@@ -0,0 +1,11 @@
+# arithmetic operators
+
+friends = 0
+# How to add 1 to a variable
+friends = friends + 1
+# Short version, but is same as friends = friends + 1
+friends +=1
+
+print(friends)
+
+# Kommet til 1:00 i videoen
